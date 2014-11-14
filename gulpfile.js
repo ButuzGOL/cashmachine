c881e0e371e4491543ddd4e8db7ca80f0f366644
ButuzGOL <ButuzGOL@gmail.com>
2014-11-14 08:39:03 +0200

    Build main

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@
 
 var gulp = require('gulp'),
     $ = require('gulp-load-plugins')(),
+    runSequence = require('run-sequence'),
     apps = ['marionette', 'ember'];
 
 gulp.task('clean', function() {
-  gulp.src('dist', { read: false })
+  return gulp.src('dist', { read: false })
     .pipe($.clean());
 });
 
 gulp.task('install', function() {
-  gulp.src('./apps/{' + apps.join(',') + '}/package.json')
+  return gulp.src('./apps/{' + apps.join(',') + '}/package.json')
     .pipe($.install());
 });
 
 gulp.task('move', function() {
-  gulp.src('./apps/{' + apps.join(',') + '}/dist/**/*')
+  return gulp.src('./apps/{' + apps.join(',') + '}/dist/**/*')
     .pipe($.rename(function(path) {
       var parts = path.dirname.split('/');
       parts.splice(1, 1);
@@ -25,4 +26,17 @@ gulp.task('move', function() {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('move-main', function() {
+  return gulp.src(['index.html'])
+    .pipe(gulp.dest('dist'));
+});
 
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe($.ghPages());
+});
+
+
+gulp.task('build', ['clean'], function() {
+  runSequence('install', ['move', 'move-main'], 'deploy');
+});

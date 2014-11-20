60139d26180709b165dae92321241aeb9d68eb21
Travis-CI <travis@nodemeatspace.com>
2014-11-20 19:19:22 +0000

    Update 2014-11-20T19:19:21.119Z

--- marionette/js/apps/header/list/controller.js
+++
@@ -1,9 +0,0 @@
-CashMachine.module('HeaderApp.List', function(List, CashMachine) {
-  List.Controller = {
-    listHeader: function() {
-      var header = new List.Header();
-
-      CashMachine.headerRegion.show(header);
-    }
-  };
-});
--- marionette/js/apps/menu/list/controller.js
+++
@@ -1,9 +0,0 @@
-CashMachine.module('MenuApp.List', function(List, CashMachine, Backbone, Marionette, $, _) {
-  List.Controller = {
-    listMenu: function() {
-      var view = new List.Menu();
-
-      CashMachine.mainRegion.show(view);
-    }
-  };
-});
